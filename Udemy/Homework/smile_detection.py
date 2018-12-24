7cb0870c79283a83dcfdaafd56b2b31bd974ba63
greengangsta <36106756+greengangsta@users.noreply.github.com>
2018-12-25 00:05:02 +0530

    Update smile_detection.py

--- Udemy/Homework/smile_detection.py
+++ Udemy/Homework/smile_detection.py
@@ -15,10 +15,10 @@ def detect(gray,frame) :
   cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
   roi_gray= gray[y:y+h, x:x+w]
   roi_color= frame[y:y+h, x:x+w]
-  eyes = eye_cascade.detectMultiScale(roi_gray,1.1,3)
+  eyes = eye_cascade.detectMultiScale(roi_gray,1.1,22)
   for (ex,ey,ew,eh) in eyes :
    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-  smiles = smile_cascade.detectMultiScale(roi_gray,1.1,3)
+  smiles = smile_cascade.detectMultiScale(roi_gray,1.7,22)
   for (sx,sy,sw,sh) in smiles :
    cv2.rectangle(roi_color,(sx,sy),(sx+sw,sy+sh),(0,0,255),2) 
  return frame
